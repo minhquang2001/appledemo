0e95e2d3a4254808f2e6f9bbbb70058712fc0ce4
QuangBoo <19522099@gm.uit.edu.vn>
2022-09-26 22:40:55 +0700

    update icon

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -2,7 +2,8 @@ import classNames from "classnames/bind";
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBagShopping, faSearch } from "@fortawesome/free-solid-svg-icons";
-
+import React, { useEffect, useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 
 import config from "~/config";
 import images from "~/assets/images";
@@ -11,6 +12,13 @@ import styles from './Header.module.scss'
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3])
+        }, 3000)
+    })
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -26,8 +34,23 @@ function Header() {
                     <Link to={config.routes.home} className={cx('list-product')}>Dịch Vụ</Link>
                 </div>
                 <div className={cx('wrap-icon')}>
-                    <FontAwesomeIcon className={cx('icon')} icon={faSearch} />
+
+                    <Tippy
+                        interactive
+                        visible={searchResult.length > 0}
+                        render={attrs => (
+                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                Ket qua
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search-box')}>
+                            <input type="text" placeholder="Search" />
+                            <FontAwesomeIcon className={cx('icon')} icon={faSearch} />
+                        </div>
+                    </Tippy>
                     <FontAwesomeIcon className={cx('icon')} icon={faBagShopping} />
+
                 </div>
             </div>
         </div>
