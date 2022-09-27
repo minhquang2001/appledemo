d05f1d17c2cf2fb25a481687cd719762c642c770
QuangBoo <19522099@gm.uit.edu.vn>
2022-09-27 22:26:37 +0700

    update search

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -13,12 +13,29 @@ const cx = classNames.bind(styles)
 
 function Header() {
     const [searchResult, setSearchResult] = useState([])
+    const [isActive, setIsActive] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
             setSearchResult([1, 2, 3])
         }, 3000)
     })
+    const searchToggle = event => {
+        setIsActive(current => !current)
+    }
+    // function searchToggle(obj) {
+    //     var container = $(obj).closest('.search-wrapper');
+    //     if (!container.hasClass('active')) {
+    //         container.addClass('active');
+    //     }
+    //     else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+    //         container.removeClass('active');
+    //         // clear input
+    //         container.find('.search-input').val('');
+    //     }
+    // }
+    const active = isActive ? 'avtive' : ''
+    const classes = cx('search-wrapper', { active })
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -44,9 +61,16 @@ function Header() {
                             </div>
                         )}
                     >
-                        <div className={cx('search-box')}>
+                        {/* <div className={cx('search-box')}>
                             <input type="text" placeholder="Search" />
                             <FontAwesomeIcon className={cx('icon')} icon={faSearch} />
+                        </div> */}
+                        <div className={classes}>
+                            <div className={cx("input-holder")}>
+                                <input type="text" className={cx("search-input")} placeholder="Type to search" />
+                                <button className={cx("search-icon")} onClick={searchToggle}><span></span></button>
+                            </div>
+                            <span className={cx("close")} onClick={searchToggle}></span>
                         </div>
                     </Tippy>
                     <FontAwesomeIcon className={cx('icon')} icon={faBagShopping} />
