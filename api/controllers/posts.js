81696a94a9db81bd87116dfc235896335c60013b
QuangBoo <19522099@gm.uit.edu.vn>
2022-10-19 14:30:06 +0700

    deploy

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 
 export const getPosts = (req,res) => {
     const q = req.query.cat
-        ? "SELECT * FROM posts WHERE group=?"
+        ? "SELECT * FROM posts WHERE cat=?"
         : "SELECT * FROM posts";
     db.query(q, [req.query.cat], (err, data) => {
         if(err) return res.status(500).send(err);
@@ -13,7 +13,7 @@ export const getPosts = (req,res) => {
 
 export const getPost = (req, res) => {
     const q =
-        "SELECT p.id, `username`, `name`, `price`, p.img, u.img AS userImg, `group`, `date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+        "SELECT p.id, `username`, `name`, `price`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
     
     db.query(q, [req.params.id], (err, data) => {
         if (err) return res.status(500).json(err);
